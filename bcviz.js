52a912d4ea0b2007f1688cb13a947a46cd7c3852
b0ri5 <borisb0ri5@gmail.com>
2013-10-17 14:48:09 -0700

    Define exports in case it doesn't exist (for browser inclusion)

--- bcviz.js
+++ bcviz.js
@@ -2,4 +2,7 @@ var bcDateTimeToDate = function(bcDateTime) {
   return bcDateTime.substring(0, 10);
 }
 
-module.exports.bcDateTimeToDate = bcDateTimeToDate;
+if(typeof exports == 'undefined'){
+    var exports = this['mymodule'] = {};
+}
+exports.bcDateTimeToDate = bcDateTimeToDate;
